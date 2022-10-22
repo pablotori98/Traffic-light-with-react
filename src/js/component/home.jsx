337e4c63b72b334beeeb59ba6bb0e090fa8c8a33
pablotori98 <pablotori98@gmail.com>
2022-10-22 11:06:12 +0000

    entrega 4geeks

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	
+	 const [color, setColor] = useState("")
+
+	const colorElegido = (color) =>{
+		 setColor(color)
+
+	}
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div><h2>Semaforo con botones de activación</h2></div>
+			<div className="semaforo">
+				<div className="fondoVerde" style={{backgroundColor:color == "green" ? "#7CFC00" : "#013220"  }} ></div>
+				<div className="fondoAmarillo" style={{backgroundColor:color == "yellow" ? "yellow" : "#999900"  }} ></div>
+				<div className="fondoRojo" style={{backgroundColor:color == "red" ? "red" : "#8B0000"  }} ></div>			
+			</div>
+
+			<div className="centerButton">
+				{/* Creamos un boton enlazado a la función colorElegido
+				Le introducimos el color que queremos que nos muestre
+				Posteriormente creamos los divs que se encuentran arriba y creamos un condicional ternario
+				en el que con un estilo Inline le decimos que si el color corresponde al obtenido en la función nos 
+				muestre el color claro para mostrar el semaforo encendido y en caso contrario que nos muestre el color oscuro */}
+				<button className="btnGreen" onClick={()=> colorElegido("green")}>Pase</button> 
+				<button className="btnYellow" onClick={() => colorElegido("yellow")}>Precaución</button>
+				<button className="btnRed" onClick={()=> colorElegido("red")}>Stop</button>
+			</div>
+			
+			<div><h2>Boton en las luces del semaforo</h2></div>
+			<div className="semaforo">
+				<button className="fondoVerde" style={{backgroundColor:color == "green" ? "#7CFC00" : "#013220"  }}
+				onClick={()=> colorElegido("green")}></button>
+				<button className="fondoAmarillo" style={{backgroundColor:color =="yellow" ? "yellow" : "#999900"}}
+				onClick={()=> colorElegido("yellow")}></button>
+				<button className="fondoRojo" style={{backgroundColor:color == "red"? "red": "#8B0000"}}
+				onClick={()=> colorElegido("red")} ></button>
+			</div>
 		</div>
 	);
 };
